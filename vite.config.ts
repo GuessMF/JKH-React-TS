e5a69776bd93502ddcc496dfa6a605a73e698536
Sergey <segas95@yandex.ru>
2024-06-05 19:13:12 +0300

    fd

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //  base: '/JKH-React-TS/',
+  base: '/JKH-React-TS/',
   // server: {
   //   proxy: {
   //     '/api': {
