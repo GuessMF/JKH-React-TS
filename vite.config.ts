4ca4510445c450aafd3b53a0811eb9c0ef1f9902
Sergey <segas95@yandex.ru>
2024-06-05 19:12:22 +0300

    meta add

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,25 @@
-import {defineConfig} from "vite";
-import react from "@vitejs/plugin-react";
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/JKH-React-TS/",
+  //  base: '/JKH-React-TS/',
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://showroom.eis24.me',
+  //       changeOrigin: true,
+  //       rewrite: (path) => {
+  //         console.log('Rewriting path:', path); // Add this line for debugging
+  //         return path.replace(/^\/api/, '');
+  //       },
+  //       configure: (proxy, options) => {
+  //         proxy.on('proxyReq', (proxyReq, req, res) => {
+  //           console.log('Proxying request:', req.url); // Add this line for debugging
+  //         });
+  //       },
+  //     },
+  //   },
+  // },
 });
